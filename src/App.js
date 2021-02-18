ec2bda896f4b9a99ca27e21b5fec994ea81cd3a0
MarcLall <marclallau@gmail.com>
2021-02-18 12:32:27 +0100

    creation page select

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react'
+import SelectTeam from './screen/SelectTeam'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <SelectTeam />
     </div>
   );
 }
---
+++ src/screen/SelectTeam.js
@@ -0,0 +1,76 @@
+import React from 'react'
+
+import './SelectTeam.css';
+
+export default function SelectTeam() {
+    return (
+        <div>
+            <div className="titleSelect">
+                Choose your hero
+            </div>
+            <div className='selectHero'>
+                <div className='card'>
+                    Card
+                </div>
+                <div className='card'>
+                    Card1
+                </div>
+                <div className='card'>
+                    Card2
+                </div>
+                <div className='card'>
+                    Card3
+                </div>
+                <div className='card'>
+                    Card4
+                </div>
+                <div className='card'>
+                    Card5
+                </div>
+                <div className='card'>
+                    Card
+                </div>
+                <div className='card'>
+                    Card1
+                </div>
+                <div className='card'>
+                    Card2
+                </div>
+                <div className='card'>
+                    Card3
+                </div>
+                <div className='card'>
+                    Card4
+                </div>
+                <div className='card'>
+                    Card5
+                </div>
+            </div>
+            <div>
+                Your Hero
+            </div>
+            <div className='selectedHero'>
+                <div className='card'>
+                    Card1 selected
+                </div>
+                <div className='card'>
+                    Card2 selected
+                </div>
+                <div className='card'>
+                    Card3 selected
+                </div>
+                <div className='card'>
+                    Card4 selected
+                </div>
+                <div className='card'>
+                    Card5 selected
+                </div>
+                <div className='card'>
+                    Card6 selected
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
